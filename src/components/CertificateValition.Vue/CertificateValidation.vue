d93c0f9fd9cddf3afb84c4fa3cc1109558b7a7f1
Gautam rangan P <1000063863@hexaware.com>
2024-06-26 14:44:04 +0530

    certificate

--- src/components/CertificateValition.Vue/CertificateValidation.vue
+++ src/components/CertificateValition.Vue/CertificateValidation.vue
@@ -1,5 +1,17 @@
 <template>
     <div>
+      <div class="row">
+        <label>Name</label>
+        <InputText type="text" v-model="empName" />
+      </div>
+      <div class="row">
+        <label>EMP ID</label>
+        <InputText type="text" v-model="empId" />
+      </div>
+      <div class="row">
+        <label>Email</label>
+        <InputText type="text" v-model="empEmail" />
+      </div>
         <div class="row">
           <label>Upload Attendance</label>
           <PDFExtractor @metaDataFromPdf= "handleDate"/>
@@ -18,6 +30,9 @@ import PDFExtractor from '../PDFExtractor/PDFExtractor.vue';
 export default {
   data(){
     return{
+      empName:'',
+      empId:'',
+      empEmail:'',
       creationDate:null,
     }
   },
@@ -25,25 +40,19 @@ export default {
     PDFExtractor
   },
   methods:{
-       formattedCreationDate(timestamp){
-// const timestamp = "D:20231113075823+00'00'";
-
-// Extract the date portion from the string
-const dateStr = timestamp.substring(2, 10);
-
-// Parse the date string into a Date object
-const year = dateStr.substring(0, 4);
-const month = dateStr.substring(4, 6);
-const day = dateStr.substring(6, 8);
-
-// Format the date in DD/MM/YYYY format
-const formattedDate = `${day}/${month}/${year}`;
-    return formattedDate
-    },
-    handleDate(timestamp){
-      this.creationDate = this.formattedCreationDate(timestamp)
-    },
-  }
+      formattedCreationDate(timestamp){
+        const dateStr = timestamp.substring(2, 10);
+        const year = dateStr.substring(0, 4);
+        const month = dateStr.substring(4, 6);
+        const day = dateStr.substring(6, 8);
+        // const formattedDate = `${day}/${month}/${year}`;
+        const formattedDate = `${month}/${day}/${year}`;
+        return formattedDate
+      },
+      handleDate(timestamp){
+        this.creationDate = this.formattedCreationDate(timestamp)
+      },
+    }
 };
 </script>
   <style>
